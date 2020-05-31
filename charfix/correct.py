b3d9f1e8195f1a5c7d5428a246699387b6d28960
LuxMiranda <19409711+LuxMiranda@users.noreply.github.com>
2020-05-31 12:56:45 -0600

    Initial anomaly detection

--- charfix/correct.py
+++ charfix/correct.py
@@ -1,5 +1,7 @@
 from symspellpy import SymSpell, Verbosity
-
+import numpy as np
+import pandas as pd 
+"""
 sym_spell = SymSpell()
 corpus_path = 'specials.txt'
 sym_spell.create_dictionary(corpus_path)
@@ -10,9 +12,17 @@ suggestions = sym_spell.lookup(input_term, Verbosity.CLOSEST)
 
 for suggestion in suggestions:
     print(suggestion)
+"""
 
+IN_FILE = 'mush.csv'
+COLS = [
+      'SiteName','Country','Province','Region','Continent','Source','Reference'
+    ]
+ 
 
 # Prompt the user to fix it 
+def prompt(anom):
+    print(anom)
     # List context and suggestions
     # Prompt to:
         # [#] Choose suggestion
@@ -27,9 +37,89 @@ for suggestion in suggestions:
         # [M]anual entry
             # Take manual entry
             # Change structure in-place
+    return
+
+# Simple binary search
+def binarySearch(anoms, datum, l, r):
+    mid = int((l+r)/2)
+    if l > r:
+        return mid,False
+    elif datum > anoms[mid]['anom']:
+        return binarySearch(anoms, datum, mid+1, r)
+    elif datum < anoms[mid]['anom']:
+        return binarySearch(anoms, datum, l, mid-1)
+    else:
+        return mid,True
+
+def contains(anoms, datum):
+    return binarySearch(anoms, datum, 0, len(anoms) - 1)
+
+def addContext(anoms, index, context):
+    anoms[index]['contexts'].append(context)
+    return anoms
+
+def addNewAnomaly(anoms, index, datum, context):
+    newAnom = {
+            'anom': datum,
+            'contexts' : [context]
+            }
+    return (anoms[:index] + [newAnom] + anoms[index:])
+
+def logAnomaly(anoms, datum, context):
+    index, found = contains(anoms, datum)
+    if found:
+        return addContext(anoms, index, context)
+    else:
+        return addNewAnomaly(anoms, index, datum, context)
+
+def getAnomalies(records):
+    anoms = []
+    # For every record
+    for labid in records.index:
+        # For every potentially anomalous feature type
+        for col in COLS:
+            datum = records.at[labid, col]
+            stripped = str(datum).replace(' ', '')\
+                            .replace('-', '')\
+                            .replace('/', '')\
+                            .replace(':', '')\
+                            .replace('.', '')\
+                            .replace(',', '')\
+                            .replace('"', '')\
+                            .replace("'", '')\
+                            .replace("=", '')\
+                            .replace("&", '')\
+                            .replace("°", '')\
+                            .replace("\\", '')\
+                            .replace("[", '')\
+                            .replace("]", '')\
+                            .replace("#", '')\
+                            .replace("+", '')\
+                            .replace("–", '')\
+                            .replace(';', '')\
+                            .replace('(', '')\
+                            .replace(')', '')\
+                            .replace('_', '')
+            # If it is still not alphanumeric after stripping ordinary symbols
+            if not stripped.isalnum():
+                # Add it to the anomalies
+                anoms = logAnomaly(anoms, datum, records.loc[labid])
+    return anoms
 
 
-# For every fixable field
-    # If the string contains non-alphanumeric characters and isn't in the fix-all dictionary
+def getData():
+    records = pd.read_csv(IN_FILE, low_memory=False, index_col=0)
+    anoms = getAnomalies(records)
+    return records, anoms
+
+
+def main():
+    # First, scan through the dataset to detect and store each anomaly. 
+    records, anoms = getData()
+    # Now, for every anomaly
+    for anom in anoms:
         # Prompt the user to fix it
-# Now, apply everything in the fix-all dictionary
+        prompt(anom)
+
+if __name__ == '__main__':
+    main()
